c316a27f9bde3b111a7eaeb2ea5e3823df8cf8c1
Alexandr Ivoilovskii <goodwin915@gmail.com>
2020-05-04 21:28:55 +0800

    upd

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -5,9 +5,9 @@ import com.javarush.task.task30.task3008.ConsoleHelper;
 import com.javarush.task.task30.task3008.Message;
 import com.javarush.task.task30.task3008.MessageType;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
 
@@ -43,7 +43,104 @@ public class Client {
         }
     }
 
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+        try {
+            synchronized (this) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            ConsoleHelper.writeMessage("Ошибка");
+        }
+        if (clientConnected) {
+            ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+        } else {
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+        }
+        while (clientConnected) {
+            String message = ConsoleHelper.readString();
+            if (message.equals("exit")) {
+                clientConnected = false;
+            } else if (shouldSendTextFromConsole()) {
+                sendTextMessage(message);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Client().run();
+    }
+
     public class SocketThread extends Thread {
 
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+        }
+
+        protected void informAboutAddingNewUser(String userName) {
+            ConsoleHelper.writeMessage(String.format("Участник %s присоединился к чату", userName));
+        }
+
+        protected void informAboutDeletingNewUser(String userName) {
+            ConsoleHelper.writeMessage(String.format("Участник %s покинул чат", userName));
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this) {
+                Client.this.notify();
+            }
+        }
+
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message receive = connection.receive();
+                if (receive.getType() == MessageType.NAME_REQUEST) {
+                    String userName = getUserName();
+                    Message message = new Message(MessageType.USER_NAME, userName);
+                    connection.send(message);
+                } else if (receive.getType() == MessageType.NAME_ACCEPTED) {
+                    notifyConnectionStatusChanged(true);
+                    return;
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message receive = connection.receive();
+                if (receive.getType() == MessageType.TEXT) {
+                    processIncomingMessage(receive.getData());
+                } else if (receive.getType() == MessageType.USER_ADDED) {
+                    informAboutAddingNewUser(receive.getData());
+                } else if (receive.getType() == MessageType.USER_REMOVED) {
+                    informAboutDeletingNewUser(receive.getData());
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                String serverAddress = getServerAddress();
+                int serverPort = getServerPort();
+                Socket socket = new Socket(serverAddress, serverPort);
+                connection = new Connection(socket);
+                clientHandshake();
+                clientMainLoop();
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e2) {
+                notifyConnectionStatusChanged(false);
+            } catch (ClassNotFoundException e) {
+                notifyConnectionStatusChanged(false);
+            }
+        }
     }
 }
